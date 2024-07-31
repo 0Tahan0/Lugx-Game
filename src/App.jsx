249331cfcc7cbee35eab310813c93340c4d041db
TAHAN <wwwkl545@gmail.com>
2024-07-31 13:24:37 +0300

    make scrolling smothy in wheel

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-// import Navbar from "./layouts/Navbar";
-// import Container from './Components/Container/Container'
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProductDetails from "./pages/ProductDetails";
 import Contact from "./pages/Contact";
 import Shop from "./pages/Shop/Shop";
 import Home from "./pages/Home/Home";
 function App() {
+    let custom_scroll = document.querySelector("*");
+    let scroll_timeout = undefined;
+    let ratio = 1.5; 
+    custom_scroll.addEventListener("wheel", (event) => {
+      clearTimeout(scroll_timeout);
+      let target = custom_scroll.scrollTop + event.deltaY * ratio;
+      let step = target > custom_scroll.scrollTop ? 1 : -1;
+      let frame = () => {
+        custom_scroll.scrollTop += step;
+        if (
+          (target < custom_scroll.scrollTop && step < 0) ||
+          (target > custom_scroll.scrollTop && step > 0)
+        ) {
+          scroll_timeout = setTimeout(frame, 5);
+        }
+      };
+      frame();
+    });
+
   return (
     <>
       <BrowserRouter>
