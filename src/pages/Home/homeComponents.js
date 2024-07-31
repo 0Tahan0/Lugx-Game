fbd85124c1466e66571acfffe566b4f9dd75aabe
TAHAN <wwwkl545@gmail.com>
2024-07-31 13:23:38 +0300

    create shorcut file

---
+++ src/pages/Home/homeComponents.js
@@ -0,0 +1,7 @@
+import Feauters from "./Feauters";
+import Landing from "./Landing";
+import LastPart from "./LastPart";
+import TopCategories from "./TopCategories";
+import TopGames from "./TopGames";
+import Trending from "./Trending";
+export { Feauters, Landing, LastPart, TopCategories, TopGames, Trending };
